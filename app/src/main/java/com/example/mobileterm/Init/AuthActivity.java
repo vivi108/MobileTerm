3780619a2e4eddf8bb6f36fd9f79d3bcc9af3421
Kim-Dong-Jun99 <kimdongjun.business@gmail.com>
2022-05-08 02:18:04 +0900

    로그인기능 중간 저장

--- app/src/main/java/com/example/mobileterm/Init/AuthActivity.java
+++ app/src/main/java/com/example/mobileterm/Init/AuthActivity.java
@@ -1,14 +1,21 @@
 package com.example.mobileterm.Init;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 import com.example.mobileterm.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 public class AuthActivity extends AppCompatActivity {
@@ -20,12 +27,28 @@ public class AuthActivity extends AppCompatActivity {
         setContentView(R.layout.activity_auth);
         ActionBar actionBar = getSupportActionBar();
         actionBar.hide();
-
+        startLoading();
         firebaseAuth = FirebaseAuth.getInstance();
         user = firebaseAuth.getCurrentUser();
         if (user != null){
-            FirebaseFirestore fv = FirebaseFirestore.getInstance();
+            FirebaseFirestore fb = FirebaseFirestore.getInstance();
+            DocumentReference documentReference = fb.collection("Users").document(user.getUid());
+            documentReference.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                @Override
+                public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                    if (task.isSuccessful()){
+                        DocumentSnapshot document = task.getResult();
+                        if (document != null){
 
+                        }
+                    }
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+
+                }
+            });
         }else{
             StartActivity(LoginActivity.class);
         }
@@ -34,5 +57,15 @@ public class AuthActivity extends AppCompatActivity {
     private void StartActivity(Class c){
         Intent intent = new Intent(this, c);
         startActivity(intent);
+    }
+
+    private void startLoading(){
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                finish();
+            }
+        }, 5000);
     }
 }
