0086aaa4042023883fdd002cdd8720d559672cfd
ZL-Selena2000 <jiyeonl2000@naver.com>
2022-05-07 16:18:33 +0900

    기존 코드들의 에러 해결 + study 페이지 및 관련 xml 추가

--- app/src/main/java/com/example/mobileterm/StudyFragment.java
+++ app/src/main/java/com/example/mobileterm/StudyFragment.java
@@ -14,7 +14,7 @@ public class StudyFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment_study, container, false);
+        ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment_study_main, container, false);
         return rootView;
     }
 }
