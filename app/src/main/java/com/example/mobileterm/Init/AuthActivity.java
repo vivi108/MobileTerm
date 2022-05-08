b88c5e574b1102a72c36670f7665788100544979
Kim-Dong-Jun99 <kimdongjun.business@gmail.com>
2022-05-08 14:45:33 +0900

    로그인 기능 구현 완료, 주석 처리 미완료

--- app/src/main/java/com/example/mobileterm/Init/AuthActivity.java
+++ app/src/main/java/com/example/mobileterm/Init/AuthActivity.java
@@ -23,6 +23,7 @@ import com.google.firebase.firestore.FirebaseFirestore;
 public class AuthActivity extends AppCompatActivity {
     private FirebaseAuth firebaseAuth;
     private FirebaseUser user;
+    private static final String TAG = "AuthActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,30 +34,37 @@ public class AuthActivity extends AppCompatActivity {
         firebaseAuth = FirebaseAuth.getInstance();
         user = firebaseAuth.getCurrentUser();
         if (user != null){
+            Log.d(TAG, "user not null");
             FirebaseFirestore fb = FirebaseFirestore.getInstance();
             DocumentReference documentReference = fb.collection("Users").document(user.getUid());
             documentReference.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
                 @Override
                 public void onComplete(@NonNull Task<DocumentSnapshot> task) {
                     if (task.isSuccessful()){
+                        Log.d(TAG, "task success");
                         DocumentSnapshot document = task.getResult();
                         if (document != null){
                             if(document.exists()){
+                                Log.d(TAG,"Should start main");
                                 StartActivity(MainActivity.class);
                             }else{
+                                Log.d(TAG, "should start login");
                                 FirebaseAuth.getInstance().signOut();
                                 StartActivity(LoginActivity.class);
                             }
+                        }else{
+                            Log.d(TAG,"document null");
                         }
                     }
                 }
             }).addOnFailureListener(new OnFailureListener() {
                 @Override
                 public void onFailure(@NonNull Exception e) {
-                    Log.e("login","Failure: null value");
+                    Log.e(TAG,"Failure: null value");
                 }
             });
         }else{
+            Log.d(TAG, "user null");
             StartActivity(LoginActivity.class);
         }
     }
