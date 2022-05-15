ac10cdfe50f3f8a94f0d71bf93a47ba55481d51e
vivi108 <118ssun@naver.com>
2022-05-15 19:22:56 +0900

    20220515_파이어베이스 스토리지

--- app/src/main/java/com/example/mobileterm/MyHomeFragment.java
+++ app/src/main/java/com/example/mobileterm/MyHomeFragment.java
@@ -3,6 +3,8 @@ package com.example.mobileterm;
 import static android.content.ContentValues.TAG;
 
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -13,55 +15,75 @@ import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
+import com.bumptech.glide.Glide;
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
 import java.util.List;
 import java.util.Objects;
 
 public class MyHomeFragment extends Fragment {
-    FirebaseFirestore db;
+    Uri imageUri;
     ImageView setting;
     ImageView profile;
     TextView name;
     ListView listview;
     String[] data ={"관심스터디","관심게시글"};
+    String uid;
+    //Firebase로 로그인한 사용자 정보 알기 위해
+    private FirebaseAuth mAuth;
+    private FirebaseUser user;
+
+    //프로필 uri이용해 bitmap으로
+    Bitmap bitmap;
+
+    public MyHomeFragment() {
+        // Required empty public constructor
+    }
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment_my_home, container, false);
         setting = (ImageView) rootView.findViewById(R.id.my_home_setting_iv);
-        listview =(ListView) rootView.findViewById(R.id.my_home_listview);
-        profile =(ImageView) rootView.findViewById(R.id.my_home_profile_iv);
-        name=(TextView)rootView.findViewById(R.id.my_home_profile_name_tv);
+        listview = (ListView) rootView.findViewById(R.id.my_home_listview);
+        profile = (ImageView) rootView.findViewById(R.id.my_home_profile_iv);
+        name = (TextView) rootView.findViewById(R.id.my_home_profile_name_tv);
 
-        //DB
-        //Cloud Firestore초기화
-        db= FirebaseFirestore.getInstance();
-        db.collection("Users").get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
-            @Override
-            public void onComplete(@NonNull Task<QuerySnapshot> task) {
-                if (task.isSuccessful()) {
-                    for (QueryDocumentSnapshot document : task.getResult()) {
-                        Log.d(TAG, document.getId() + " => " + document.getData());
-                    }
-                } else {
-                    Log.w(TAG, "Error getting documents.", task.getException());
-                }
-            }
-        });
+        //Firebase 로그인한 사용자 정보
+        mAuth = FirebaseAuth.getInstance();
+       user = FirebaseAuth.getInstance().getCurrentUser();
+        if (user != null) {
+            String name = user.getDisplayName();
+            String email = user.getEmail();
+            //Uri photoUrl = user.getPhotoUrl();
+            //boolean emailVerified = user.isEmailVerified();
+            uid= user.getUid();
+        } else {
+            // No user is signed in
+        }
+        loadImage(uid);
+
 
         //리스트뷰
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(requireActivity(), android.R.layout.simple_list_item_1,data);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(requireActivity(), android.R.layout.simple_list_item_1, data);
         listview.setAdapter(adapter);
 
         //톱니바퀴, 설정 그림 눌렀을 때
@@ -97,17 +119,67 @@ public class MyHomeFragment extends Fragment {
         });
 
         //리스트뷰 메뉴 선택했을 때
-        listview.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+        listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
-            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
-
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> adapterView) {
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
 
             }
         });
         return rootView;
+    }
+    //서버에서 기본 이미지 로드하기
+    public void loadbasicImage() {
+
+        FirebaseStorage storage = FirebaseStorage.getInstance("gs://mptermproject-75489.appspot.com"); //firebase storate 경로
+        StorageReference storageRef = storage.getReference();
+        if (storageRef.child("profile_image/ic_profile_gray.jpg") != null) {
+            storageRef.child("profile_image/ic_profile_gray.jpg").getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                @Override
+                public void onSuccess(Uri uri) {
+                    //이미지 로드 성공시
+
+
+                    Glide.with(getActivity())
+                            .load(uri)
+                            .into(profile);
+
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception exception) {
+                    //이미지 로드 실패시
+                    Toast.makeText(getActivity(), "실패", Toast.LENGTH_SHORT).show();
+
+                }
+            });
+        }
+    }
+    //서버에서 사용자 지정 프로필 이미지 로드하기
+    public void loadImage(String uid){
+        FirebaseStorage storage = FirebaseStorage.getInstance("gs://mptermproject-75489.appspot.com"); //firebase storate 경로
+        StorageReference storageRef = storage.getReference();
+
+        //파일 명 만들기
+        String filename = "profile" +uid +".jpg"; //ex) profile1.jpg 로그인 하는 사람의 식별값에 맞는 사진 가져오기
+        if(storageRef.child("profile_image/"+filename)!=null) {
+            storageRef.child("profile_image/" + filename).getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                @Override
+                public void onSuccess(Uri uri) {
+                    //이미지 로드 성공시
+
+                    Glide.with(getActivity())
+                            .load(uri)
+                            .into(profile);
+
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception exception) {
+                    //이미지 로드 실패시
+                    //Toast.makeText(getApplicationContext(), "실패", Toast.LENGTH_SHORT).show();
+                    loadbasicImage();
+                }
+            });
+        }
     }
 }
