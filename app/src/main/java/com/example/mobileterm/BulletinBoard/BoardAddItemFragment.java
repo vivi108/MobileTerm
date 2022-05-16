e7e54bc2de006374da348e25e52fcedc0b289e79
Kim-Dong-Jun99 <kimdongjun.business@gmail.com>
2022-05-16 10:42:48 +0900

    게시물 추가는 되는데, 프레그멘트 화면전환에서 오류남

--- app/src/main/java/com/example/mobileterm/BulletinBoard/BoardAddItemFragment.java
+++ app/src/main/java/com/example/mobileterm/BulletinBoard/BoardAddItemFragment.java
@@ -18,10 +18,13 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.WriteBatch;
 
+import java.util.Collection;
 import java.util.Locale;
 
 public class BoardAddItemFragment extends Fragment {
@@ -40,8 +43,10 @@ public class BoardAddItemFragment extends Fragment {
     String content;
     String title;
     String tags;
-
+    String[] tagIter;
     String TAG = "BoardAddItem";
+    BoardInfo newBoardItem;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -57,10 +62,11 @@ public class BoardAddItemFragment extends Fragment {
 
         endWriteButton = rootView.findViewById(R.id.endWriteButton);
 
+
         endWriteButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                db.collection("User").document(user.getUid()).get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                db.collection("Users").document(user.getUid()).get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
                     @Override
                     public void onComplete(@NonNull Task<DocumentSnapshot> task) {
                         if (task.isSuccessful()) {
@@ -73,7 +79,9 @@ public class BoardAddItemFragment extends Fragment {
                             title = titleEditText.getText().toString();
                             content = bodyEditText.getText().toString();
                             tags = tagEditText.getText().toString();
-                            String[] tagIter = tags.split(" ");
+                            tagIter = tags.split(" ");
+
+
                             BulletinBoardCollection newItem = new BulletinBoardCollection(content, nickname, "default",title);
                             db.collection("BulletinBoard").add(newItem).addOnCompleteListener(new OnCompleteListener<DocumentReference>() {
                                 @Override
@@ -82,21 +90,27 @@ public class BoardAddItemFragment extends Fragment {
                                         DocumentReference document = task.getResult();
                                         Log.e(TAG, "did:"+document.getId());
                                         did = document.getId();
-                                        for (String tag: tagIter){
-                                            BoardTags newTag = new BoardTags(tag.substring(1));
-                                            db.collection("BulletinBoard").document(did).collection("BoardTags").add(newTag).addOnCompleteListener(new OnCompleteListener<DocumentReference>() {
-                                                @Override
-                                                public void onComplete(@NonNull Task<DocumentReference> task) {
-                                                    if (task.isSuccessful()){
-                                                        Log.e(TAG,"tag add success");
-                                                    }
-                                                }
-                                            });
+                                        newBoardItem = new BoardInfo(title, content, nickname, did);
+                                        WriteBatch batch = db.batch();
+                                        for (String tag: tagIter) {
+                                            DocumentReference tempref = db.collection("BulletinBoard").document(did).collection("BoardTags").document(tag);
+                                            BoardTags newTag = new BoardTags(tag);
+                                            batch.set(tempref, newTag);
                                         }
-                                        BoardFragment boardFragment = new BoardFragment();
-                                        BoardInfo newBoardItem = new BoardInfo(title, content, nickname, did);
-                                        boardFragment.addNewItem(newBoardItem);
-                                        getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.main_frame_layout, boardFragment).addToBackStack(null).commit();
+
+                                        batch.commit().addOnCompleteListener(new OnCompleteListener<Void>() {
+                                            @Override
+                                            public void onComplete(@NonNull Task<Void> task) {
+                                                if (task.isSuccessful()) {
+                                                    Log.e(TAG,"tag add success");
+                                                    fragmentChange();
+                                                }
+                                            }
+                                        });
+
+
+
+
                                     }
                                 }
                             });
@@ -108,5 +122,11 @@ public class BoardAddItemFragment extends Fragment {
 
         return rootView;
 
+    }
+
+    public void fragmentChange() {
+        BoardFragment boardFragment = new BoardFragment();
+        boardFragment.addNewItem(newBoardItem);
+        getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.main_frame_layout, boardFragment).addToBackStack(null).commit();
     }
 }
