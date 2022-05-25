824772d11e68238f84177e93f8a28de0e3a5f66c
Kim-Dong-Jun99 <kimdongjun.business@gmail.com>
2022-05-25 15:18:44 +0900

    진짜 끝..................

--- app/src/main/java/com/example/mobileterm/BulletinBoard/BoardFragment.java
+++ app/src/main/java/com/example/mobileterm/BulletinBoard/BoardFragment.java
@@ -48,6 +48,8 @@ public class BoardFragment extends Fragment {
     ArrayList<BoardInfo> arrayList = new ArrayList<BoardInfo>();
     ArrayList<String> tagList = new ArrayList<String>();
     ArrayList<String> didList = new ArrayList<String>();
+    ArrayList<String> totalDids = new ArrayList<String>();
+
     Dialog filterDialog;
     EditText tagSearchEditText;
     ImageButton tagSearchButton;
@@ -74,12 +76,23 @@ public class BoardFragment extends Fragment {
 
         ArrayList<BoardInfo> newArrayList = new ArrayList<BoardInfo>();
 
+        tagSearchEditText = filterDialog.findViewById(R.id.tagSearchEditText);
+        tagSearchButton = filterDialog.findViewById(R.id.tagSearchButton);
+        tagEraseButton = filterDialog.findViewById(R.id.tagEraseButton);
+        appliedTagsTextView = filterDialog.findViewById(R.id.appliedTagsTextView);
+        orderRecent = filterDialog.findViewById(R.id.orderRecent);
+        orderLike = filterDialog.findViewById(R.id.orderLike);
+        orderOld = filterDialog.findViewById(R.id.orderOld);
+        radioGroup = filterDialog.findViewById(R.id.radioGroup);
+
         db.collection("BulletinBoard").get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
             @Override
             public void onComplete(@NonNull Task<QuerySnapshot> task) {
                 if (task.isSuccessful()){
                     for (QueryDocumentSnapshot document : task.getResult()) {
                         if (document.exists()) {
+                            didList.add(document.getId());
+                            totalDids.add(document.getId());
                             String did = (String) document.getId();
                             String title = (String) document.getData().get("title");
                             String content = (String) document.getData().get("content");
@@ -112,22 +125,67 @@ public class BoardFragment extends Fragment {
                     rootView.findViewById(R.id.searchButton).setOnClickListener(onClickListener);
                     rootView.findViewById(R.id.filterButton).setOnClickListener(onClickListener);
                     rootView.findViewById(R.id.startWriteButton).setOnClickListener(onClickListener);
-                    if (searchEditText.getText().toString().length() > 0){
-                        adapter.filter(searchEditText.getText().toString());
+
+                    if (tagSearchEditText.getText().toString().length() == 0 && searchEditText.getText().toString().length() == 0){
+                        // 정렬만 하면된다
+                        adapter.reorder(radioGroup.getCheckedRadioButtonId());
+                    }else if (tagSearchEditText.getText().toString().length() == 0 && searchEditText.getText().toString().length() > 0){
+                        // 검색하고 정렬 확인해서 하면된다,
+                        adapter.filter(searchEditText.getText().toString(), radioGroup.getCheckedRadioButtonId());
+                    }else if (tagSearchEditText.getText().toString().length() > 0 && searchEditText.getText().toString().length() == 0){
+                        // 정렬이랑 태그만
+                        tagList = new ArrayList<String>();
+                        didList = new ArrayList<String >();
+                        for (String tempTag: tagSearchEditText.getText().toString().split("#")){
+                            if (tempTag.length() > 0){
+                                tagList.add("#"+tempTag.trim());
+                            }
+                        }
+                        appliedTagsTextView.setText("설정된 태그: "+tagSearchEditText.getText().toString());
+                        db.collectionGroup("tagDocs").get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                            @Override
+                            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                                if (task.isSuccessful()){
+                                    QuerySnapshot querySnapshot = task.getResult();
+                                    for (DocumentSnapshot document : querySnapshot){
+                                        if (tagList.contains((String) document.getData().get("tag")) && !didList.contains((String) document.getData().get("did"))){
+                                            didList.add((String) document.getData().get("did"));
+                                        }
+                                    }
+                                    adapter.filter(didList,radioGroup.getCheckedRadioButtonId());
+//                                    filterDialog.dismiss();
+                                }
+                            }
+                        });
+                    }else{
+                        // 전부 확인해야함
+                        tagList = new ArrayList<String>();
+                        didList = new ArrayList<String >();
+                        for (String tempTag: tagSearchEditText.getText().toString().split("#")){
+                            if (tempTag.length() > 0){
+                                tagList.add("#"+tempTag.trim());
+                            }
+                        }
+                        appliedTagsTextView.setText("설정된 태그: "+tagSearchEditText.getText().toString());
+                        db.collectionGroup("tagDocs").get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                            @Override
+                            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                                if (task.isSuccessful()){
+                                    QuerySnapshot querySnapshot = task.getResult();
+                                    for (DocumentSnapshot document : querySnapshot){
+                                        if (tagList.contains((String) document.getData().get("tag")) && !didList.contains((String) document.getData().get("did"))){
+                                            didList.add((String) document.getData().get("did"));
+                                        }
+                                    }
+                                    adapter.searchTagReorder(searchEditText.getText().toString(),didList,radioGroup.getCheckedRadioButtonId());
+//                                    filterDialog.dismiss();
+                                }
+                            }
+                        });
                     }
                 }
             }
         });
-
-
-//        db.collectionGroup("BoardTags").get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
-//            @Override
-//            public void onComplete(@NonNull Task<QuerySnapshot> task) {
-//                if (task.isSuccessful()){
-//
-//                }
-//            }
-//        });
 
 
         return rootView;
@@ -139,7 +197,7 @@ public class BoardFragment extends Fragment {
             switch (view.getId()){
                 case R.id.searchButton:
                     String search = searchEditText.getText().toString();
-                    adapter.filter(search);
+                    adapter.filter(search, radioGroup.getCheckedRadioButtonId());
                     break;
                 case R.id.filterButton:
                     showDialog();
@@ -153,37 +211,28 @@ public class BoardFragment extends Fragment {
         }
     };
 
-    public void addNewItem(BoardInfo newItem){
-        adapter.addItem(newItem);
-    }
 
     public void showDialog(){
         filterDialog.show();
-        tagSearchEditText = filterDialog.findViewById(R.id.tagSearchEditText);
-        tagSearchButton = filterDialog.findViewById(R.id.tagSearchButton);
-        tagEraseButton = filterDialog.findViewById(R.id.tagEraseButton);
-        appliedTagsTextView = filterDialog.findViewById(R.id.appliedTagsTextView);
-        orderRecent = filterDialog.findViewById(R.id.orderRecent);
-        orderLike = filterDialog.findViewById(R.id.orderLike);
-        orderOld = filterDialog.findViewById(R.id.orderOld);
-        radioGroup = filterDialog.findViewById(R.id.radioGroup);
         tagSearchButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Log.d(TAG,"tagSearch pressed");
                 String raw = tagSearchEditText.getText().toString();
                 if (raw.length() == 0){
+                    //검색 정렬만
                     adapter.reorder(radioGroup.getCheckedRadioButtonId());
-                    appliedTagsTextView.setText("설정된 태그: ");
-                    radioGroup.check(R.id.orderRecent);
                     filterDialog.dismiss();
                 }else{
+                    //검색 정렬 태그 모두 다
+                    tagList = new ArrayList<String>();
+                    didList = new ArrayList<String >();
                     for (String tempTag:raw.split("#")){
                         if (tempTag.length() > 0){
                             tagList.add("#"+tempTag.trim());
                         }
                     }
-                    appliedTagsTextView.setText(appliedTagsTextView.getText().toString()+" "+raw);
+                    appliedTagsTextView.setText("설정된 태그: "+raw);
                     db.collectionGroup("tagDocs").get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
                         @Override
                         public void onComplete(@NonNull Task<QuerySnapshot> task) {
@@ -194,18 +243,12 @@ public class BoardFragment extends Fragment {
                                         didList.add((String) document.getData().get("did"));
                                     }
                                 }
-                                tagSearchEditText.setText("");
-//                            adapter.reorder(radioGroup.getCheckedRadioButtonId());
                                 adapter.filter(didList,radioGroup.getCheckedRadioButtonId());
-                                appliedTagsTextView.setText("설정된 태그: ");
-                                radioGroup.check(R.id.orderRecent);
                                 filterDialog.dismiss();
                             }
                         }
                     });
                 }
-
-
 
             }
         });
@@ -213,9 +256,12 @@ public class BoardFragment extends Fragment {
         tagEraseButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                didList = new ArrayList<String>();
+                // 태그를 지우는데 검색이랑 정렬은 적용
+                didList = totalDids;
+                tagSearchEditText.setText("");
+                tagSearchEditText.setHint("태그를 입력하세요.");
                 appliedTagsTextView.setText("설정된 태그:");
-                adapter.renew();
+                adapter.filter(searchEditText.getText().toString(), radioGroup.getCheckedRadioButtonId());
                 filterDialog.dismiss();
             }
         });
